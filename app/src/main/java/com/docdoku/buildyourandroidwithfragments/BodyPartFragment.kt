04ad82f1212eb89720e6e2ba65f2d688f7a5a7c2
Romain Ziba <romain.ziba@docdoku.com>
2018-03-26 16:49:22 +0200

    Handle click and rotation

--- app/src/main/java/com/docdoku/buildyourandroidwithfragments/BodyPartFragment.kt
+++ app/src/main/java/com/docdoku/buildyourandroidwithfragments/BodyPartFragment.kt
@@ -6,24 +6,54 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import java.util.ArrayList
 
 /**
  * Created by romainz on 26/03/18.
  */
 class BodyPartFragment : Fragment() {
 
+    companion object {
+        private val IMAGE_IDS = "IMAGE_IDS"
+        private val IMAGE_INDEX = "INDEX"
+    }
+
     private var mImageIds: List<Int> = listOf()
+    private var mImageIndex: Int = 0
 
     fun setImageIds(imageIds: List<Int>) {
         mImageIds = imageIds
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val imageIds = savedInstanceState?.getIntegerArrayList(IMAGE_IDS)
+        if (imageIds != null) {
+            mImageIds = imageIds.toList()
+        }
+        val imageIndex = savedInstanceState?.getInt(IMAGE_INDEX)
+        if (imageIndex != null) {
+            mImageIndex = imageIndex
+        }
+
         val rootView = inflater?.inflate(R.layout.fragment_body_part, container, false)
         val imageView = rootView?.findViewById(R.id.iv_body_part) as ImageView
         if (mImageIds.isNotEmpty()) {
-            imageView.setImageResource(mImageIds[0])
+            imageView.setImageResource(mImageIds[mImageIndex])
+            imageView.setOnClickListener {
+                // Increment position as long as the index remains <= the size of the image ids list
+                if (mImageIndex < mImageIds.size - 1) {
+                    mImageIndex++
+                } else {
+                    mImageIndex = 0
+                }
+                imageView.setImageResource(mImageIds[mImageIndex])
+            }
         }
         return rootView
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        outState?.putIntegerArrayList(IMAGE_IDS, mImageIds as ArrayList<Int>)
+        outState?.putInt(IMAGE_INDEX, mImageIndex)
     }
 }
--- app/src/main/java/com/docdoku/buildyourandroidwithfragments/BuildAndroidActivity.kt
+++ app/src/main/java/com/docdoku/buildyourandroidwithfragments/BuildAndroidActivity.kt
@@ -17,8 +17,6 @@ class BuildAndroidActivity : AppCompatActivity() {
      *  One container should be 180dp in height. This container will contain dynamic fragment
      *  TODO (4) Instantiate three fragments for Head, Body and Legs.
      *  In these fragments, you'd show the first image in the list of head/body/legs images
-     *
-     *  Part 2:
      *  TODO (5): While clicking on an item, change the image to display by the next one
      *  TODO (6): Save the state of the fragment to display the same images in each fragment while rotating the device
      *
@@ -55,17 +53,19 @@ class BuildAndroidActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_build_android)
 
-        val headFragment = BodyPartFragment()
-        headFragment.setImageIds(AndroidImageAssets.heads)
-        val bodyFragment = BodyPartFragment()
-        bodyFragment.setImageIds(AndroidImageAssets.bodies)
-        val legsFragment = BodyPartFragment()
-        legsFragment.setImageIds(AndroidImageAssets.legs)
+        if (savedInstanceState == null) {
+            val headFragment = BodyPartFragment()
+            headFragment.setImageIds(AndroidImageAssets.heads)
+            val bodyFragment = BodyPartFragment()
+            bodyFragment.setImageIds(AndroidImageAssets.bodies)
+            val legsFragment = BodyPartFragment()
+            legsFragment.setImageIds(AndroidImageAssets.legs)
 
-        supportFragmentManager.beginTransaction()
-                .add(R.id.head_fragment_container, headFragment)
-                .add(R.id.body_fragment_container, bodyFragment)
-                .add(R.id.legs_fragment_container, legsFragment)
-                .commit()
+            supportFragmentManager.beginTransaction()
+                    .add(R.id.head_fragment_container, headFragment)
+                    .add(R.id.body_fragment_container, bodyFragment)
+                    .add(R.id.legs_fragment_container, legsFragment)
+                    .commit()
+        }
     }
 }
